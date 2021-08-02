282a9e6e6fc793026e2fbd7c2e718c2bbade0fc7
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-08-02 19:52:33 +0200

    chore: cleanup

--- src/main/kotlin/com/jagrosh/jagtag/Environment.kt
+++ src/main/kotlin/com/jagrosh/jagtag/Environment.kt
@@ -4,31 +4,22 @@ class Environment : HashMap<String, Any>() {
 
     /** returns the value of the reified type from key or null **/
     inline fun <reified T> getReified(key: String): T? {
-        val value = super.get(key)
-        return if (value is T?) {
-            value
-        } else {
-            null
-        }
+        val value = get(key)
+        return if (value is T?) value
+        else null
     }
 
     /** returns the value of the reified type from key or the default value **/
     inline fun <reified T> getOrDefaultReified(key: String, defaultValue: T): T {
-        val any = super.getOrDefault(key, defaultValue as Any)
-        return if (any is T) {
-            any
-        } else {
-            defaultValue
-        }
+        val any = getOrDefault(key, defaultValue as Any)
+        return if (any is T) any
+        else defaultValue
     }
 
     /** returns the value of the reified type from key or throws an exception **/
     inline fun <reified T> getReifiedX(key: String): T {
-        val value = super.get(key)
-        return if (value is T) {
-            value
-        } else {
-            throw IllegalArgumentException("value is not the correct type, value: $value")
-        }
+        val value = get(key)
+        return if (value is T) value
+        else throw IllegalArgumentException("value is not the correct type, value: $value")
     }
 }
